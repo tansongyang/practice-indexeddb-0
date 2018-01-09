5ddc654ddaba72e8808ed108366f11066828b610
Frank Tan <tansongyang@gmail.com>
2018-01-08 21:43:59 -0500

    Add Dexie implementation

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Consider using the [`serve`](https://www.npmjs.com/package/serve) package:
 
     cd path/to/practice-indexeddb-0
     serve -T
+
+To try out different implementations, update the script tags in `index.html`.
+When you switch, make sure to delete your existing DB using dev tools.
---
+++ index-dexie.js
@@ -0,0 +1,39 @@
+(async function () {
+  const DB_NAME = 'db';
+  const DB_VERSION = 1;
+  const STORE_NAME = 'pokemon';
+  const STORE_KEY_PATH = 'name';
+  const GET_URI = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+
+  const db = new Dexie(DB_NAME);
+  db.version(DB_VERSION).stores({
+    [STORE_NAME]: `${STORE_KEY_PATH}, url`,
+  });
+  db.open().catch((e) => {
+    console.error(`Error while opening DB "${DB_NAME}"`);
+  });
+
+  const count = await db[STORE_NAME].count();
+  if (count > 0) {
+    console.log(`Skipping data fetch because ${count} records were found`);
+  } else {
+    console.log('Fetching data because no records were found');
+    try {
+      const response = await fetch(GET_URI);
+      const json = await response.json();
+      await db.transaction('rw', STORE_NAME, () => {
+        return db[STORE_NAME].bulkAdd(json.results);
+      });
+    } catch (e) {
+      console.log(`Error while fetching data`);
+    }
+  }
+
+  const records = await db[STORE_NAME].toArray();
+  const list = document.getElementById('list');
+  records.forEach((r) => {
+    const li = document.createElement('li');
+    li.textContent = r.name;
+    list.appendChild(li);
+  });
+})();
